a54b26bfb6cd9421e70b2ab8105ad234105d7eb5
duskull96 <pavel6900@gmail.com>
2020-11-12 16:56:37 +0300

    add posSize

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ const canvasHeight = canvas.height;
 const dot = canvas.getContext("2d");
 const pos = canvas.getContext("2d");
 
-
+const posSize = [5,4,3,2,1]
 const def = []
-def.forEach( item => {
-    pos.fillRect(item.x,item.y,5,5)
+
+def.forEach(item => {
+    pos.fillRect(item.x, item.y, 5, 5)
 })
 
 let x = 700, y = 300;
 
 
-
-
-canvas.onclick = function(event) {
+canvas.onclick = function (event) {
     let defPos = getCoords(event);
     pos.fillStyle = 'red'
     console.log(defPos);
@@ -31,9 +30,9 @@ canvas.onclick = function(event) {
         }
         def.push(defPos);
     }
-    
-    def.forEach( item => {
-        pos.fillRect(item.x,item.y,5,5)
+
+    def.forEach(item => {
+        pos.fillRect(item.x, item.y, 5, 5)
     })
 }
 
@@ -48,7 +47,7 @@ function getCoords(event) {
 }
 
 function deleteDef() {
-    def.splice(0,def.length);
+    def.splice(0, def.length);
     btnDeleteDef.classList.add('disabled');
     btnDraw.classList.add('disabled');
     pos.clearRect(0, 0, canvas.width, canvas.height);
@@ -56,18 +55,20 @@ function deleteDef() {
 
 function startDraw() {
     let countDots = document.querySelector('.count-dots').value;
-    console.log(countDots);
+    $('select').formSelect();
+    let posSizeIndex = Number($('select').formSelect('getSelectedValues').join())
+    // console.log(countDots, posSize[posSizeIndex-1]);
     for (let i = 0; i < countDots; i++) {
-        let index = rndInt(0,def.length-1)
-        console.log(x,y, index);
+        let index = rndInt(0, def.length - 1)
+        // console.log(x, y, index);
         dot.fillStyle = 'black';
-        dot.fillRect(x,y,5,5)
+        dot.fillRect(x, y, posSize[posSizeIndex-1], posSize[posSizeIndex-1])
         x < def[index].x ? x = (Math.abs(x - def[index].x) / 2) + x :
-        x = (Math.abs(x - def[index].x) / 2) + def[index].x
+            x = (Math.abs(x - def[index].x) / 2) + def[index].x
 
         y < def[index].y ? y = (Math.abs(y - def[index].y) / 2) + y :
-        y = (Math.abs(y - def[index].y) / 2) + def[index].y
-        console.log(x,y);
+            y = (Math.abs(y - def[index].y) / 2) + def[index].y
+        // console.log(x, y);
     }
 }
 
